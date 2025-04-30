90e07c1711305c97673078cbe2befc16f96410e4
orlando-orlando <150473504+orlando-orlando@users.noreply.github.com>
2025-04-30 17:51:19 -0600

    .

--- js/inicio.js
+++ js/inicio.js
@@ -23,31 +23,28 @@ function flujoInfinity(){
     document.getElementById('flujoInfinity2').innerText = 'Flujo infinity: ' + flujoInfinity2 + ' gpm';
 }
 
-function flujoCalentamiento(){
+const climaMensual = {
+    "Puerto Vallarta": [
+      { mes: "Ene", tempMax: 25.6, tempMin: 16.3, vientoMax: 27.7, humedadRel: 65 },
+      { mes: "Feb", tempMax: 26.5, tempMin: 16.9, vientoMax: 25.9, humedadRel: 64 },
+      { mes: "Mar", tempMax: 26.9, tempMin: 17.9, vientoMax: 26.6, humedadRel: 65 },
+      { mes: "Abr", tempMax: 26.7, tempMin: 18.7, vientoMax: 27.1, humedadRel: 66 },
+      { mes: "May", tempMax: 26.3, tempMin: 19.2, vientoMax: 27.2, humedadRel: 71 },
+      { mes: "Jun", tempMax: 25.3, tempMin: 19.2, vientoMax: 25.7, humedadRel: 78 },
+      { mes: "Jul", tempMax: 25.0, tempMin: 19.0, vientoMax: 23.5, humedadRel: 80 },
+      { mes: "Ago", tempMax: 25.5, tempMin: 19.0, vientoMax: 23.2, humedadRel: 79 },
+      { mes: "Sep", tempMax: 26.1, tempMin: 18.8, vientoMax: 22.8, humedadRel: 78 },
+      { mes: "Oct", tempMax: 26.4, tempMin: 18.3, vientoMax: 23.9, humedadRel: 73 },
+      { mes: "Nov", tempMax: 26.0, tempMin: 17.3, vientoMax: 25.4, humedadRel: 68 },
+      { mes: "Dic", tempMax: 25.5, tempMin: 16.4, vientoMax: 26.3, humedadRel: 65 }
+    ],
+    // Agrega más ubicaciones aquí si las tienes
+  };
 
-}
 
-function flujoMaximo(){
-
-}
-
-/*function procesarFormulario() {
-    let opcionSiNo = document.getElementById('opcionSiNo').value;
-    let calentamiento = document.getElementById('calentamiento').value;
-
-    document.getElementById('resultadoOpcionSiNo').innerText = 'Selección desborde: ' + opcionSiNo;
-    document.getElementById('resultadoCalentamiento').innerText = 'Selección calentamiento: ' + calentamiento;
-}*/
-
-/*function ubicacion1(){
-    let ubicacion1 = document.getElementById('ubicacion').value;
-    document.getElementById('ubicacion1').innerText = 'Ubicación: ' + ubicacion1;
-
-}*/
-
-const temperaturas = {
+const temperatura = {
     "guadalajara": {
-        min: [9.5, 10.3, 12.3, 14.3, 16.4, 17.3, 16.5, 16.4, 16.5, 14.9, 12.1, 10.3],
+        mes: min: [9.5, 10.3, 12.3, 14.3, 16.4, 17.3, 16.5, 16.4, 16.5, 14.9, 12.1, 10.3],
         max: [24.7, 26.5, 29, 31.2, 32.5, 30.5, 27.5, 27.3, 27.1, 27.1, 26.4, 24.7]
     },
     "mexicali": {
@@ -387,61 +384,5 @@ const humedad = {
 function masaEvaporada(){
     let {area1} = volumen();
     let tempAgua2 = document.getElementById('tempAgua').value;
-
-}
-
-function perdidaEvaporacion(){
-
-}
-
-function perdidaTuberia(){
-
-}
-
-function perdidaRadiacion(){
-
-}
-
-function perdidaConveccion(){
-
-}
-
-function perdidaTransmision(){
-
-}
-
-function perdidaInfinity(){
-
-}
-
-function tablasCalor(){
-
-}
-
-
-function velocidadFlujo() {
-    const velocidadSuccion = 5.5;
-    const velocidadDescarga = 7.5;
-    
-}
-
-function diametroTuberia() {
-
-}
-
-
-
-function retorno() {
-    let flujoVolumen2 = flujoVolumen();
-    let {area1} = volumen();
-    const flujoRetorno1 = 20;
-    const flujoRetorno2 = 40;
-    let numeroRetornos1 = parseFloat((flujoVolumen2 / flujoRetorno1).toFixed(0));
-    let numeroRetornos2 = parseFloat((flujoVolumen2 / flujoRetorno2).toFixed(0));
-    let tuberiaRetorno1 = parseFloat(((Math.sqrt(area1))*4).toFixed(1));
-    document.getElementById('tuberiaRetorno1').innerText = 'Tuberia retornos: ' + tuberiaRetorno1 + ' m';
-    document.getElementById('numeroRetornos1').innerText = 'Numero de retornos 1.5in: ' + numeroRetornos1 + ' piezas';
-    document.getElementById('numeroRetornos2').innerText = 'Numero de retornos 2.0in: ' + numeroRetornos2 + ' piezas';
-
 
 }
